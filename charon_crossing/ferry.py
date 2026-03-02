946a35163808ef93fd6bce6dd38297d96bd96623
Mark Baggett <markpbaggett@gmail.com>
2026-03-02 15:55:08 -0600

    Run as app.

--- charon_crossing/ferry.py
+++ charon_crossing/ferry.py
@@ -1,18 +1,27 @@
+import click
 from . import FedoraWork
 from .fedora import FedoraCollection
 from csv import DictWriter
 
-collection = FedoraCollection('https://api-pre.library.tamu.edu/fcrepo/rest/london-maps-spotlight-example')
-members = collection.get_members()
 
-rows = []
-for member in members:
-    work = FedoraWork(member)
-    row = {k: "|".join(v) for k, v in work.metadata_to_dict().items()}
-    row["files"] = "|".join(work.get_ordered_member_files())
-    rows.append(row)
+@click.command()
+@click.argument("uri")
+def ferry(uri):
+    collection = FedoraCollection(uri)
+    members = collection.get_members()
 
-with open(f"{collection.uri.split('/')[-1]}.csv", "w", newline="") as f:
-    writer = DictWriter(f, fieldnames=list(rows[0].keys()))
-    writer.writeheader()
-    writer.writerows(rows)
+    rows = []
+    for member in members:
+        work = FedoraWork(member)
+        row = {k: "|".join(v) for k, v in work.metadata_to_dict().items()}
+        row["files"] = "|".join(work.get_ordered_member_files())
+        rows.append(row)
+
+    with open(f"{collection.uri.split('/')[-1]}.csv", "w", newline="") as f:
+        writer = DictWriter(f, fieldnames=list(rows[0].keys()))
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+if __name__ == "__main__":
+    ferry()
