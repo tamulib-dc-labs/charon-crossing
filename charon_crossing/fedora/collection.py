796e63cc8153607d0a589420aa6a1aa64e474f66
Mark Baggett <markpbaggett@gmail.com>
2026-03-02 16:40:00 -0600

    Add get_created.

--- charon_crossing/fedora/collection.py
+++ charon_crossing/fedora/collection.py
@@ -18,7 +18,12 @@ class FedoraCollection(FedoraObject):
             subject=URIRef(self.uri), predicate=URIRef(f"{self.namespaces.ldp}contains")
         ))
 
+    def get_created(self):
+        return next(self.content.objects(subject=self.uri, predicate=URIRef(f"{self.namespaces.fedora}created")), None)
+
 
 if __name__ == '__main__':
-    x = FedoraCollection('https://api-pre.library.tamu.edu/fcrepo/rest/bb/97/f2/3e/bb97f23e-803a-4bd6-8406-06802623554c')
+    x = FedoraCollection(
+        'https://api.library.tamu.edu/fcrepo/rest/a6/f0/55/01/a6f05501-9312-4fd5-a167-f7ce2fa28ca6'
+    )
     print(x.get_members())
