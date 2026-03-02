62f66fa184f8610571789b0be1d5b6ea8b618b22
Mark Baggett <markpbaggett@gmail.com>
2026-03-02 15:48:44 -0600

    Pull metadata and files.

--- charon_crossing/ferry.py
+++ charon_crossing/ferry.py
@@ -1,3 +1,18 @@
+from . import FedoraWork
 from .fedora import FedoraCollection
-x = FedoraCollection('https://api-pre.library.tamu.edu/fcrepo/rest/bb/97/f2/3e/bb97f23e-803a-4bd6-8406-06802623554c')
-print(x.get_members())
+from csv import DictWriter
+
+collection = FedoraCollection('https://api-pre.library.tamu.edu/fcrepo/rest/london-maps-spotlight-example')
+members = collection.get_members()
+
+rows = []
+for member in members:
+    work = FedoraWork(member)
+    row = {k: "|".join(v) for k, v in work.metadata_to_dict().items()}
+    row["files"] = "|".join(work.get_ordered_member_files())
+    rows.append(row)
+
+with open(f"{collection.uri.split('/')[-1]}.csv", "w", newline="") as f:
+    writer = DictWriter(f, fieldnames=list(rows[0].keys()))
+    writer.writeheader()
+    writer.writerows(rows)
